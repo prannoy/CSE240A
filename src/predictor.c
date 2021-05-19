066bb95460cb3c675b6327d5be0cfd158f8c630c
Sachin <sachincdeshpande@yahoo.com>
2021-05-19 00:23:07 -0700

    started other way, correct table now

--- src/predictor.c
+++ src/predictor.c
@@ -5,8 +5,10 @@
 //  Implement the various branch predictors below as      //
 //  described in the README                               //
 //========================================================//
+#include <math.h>
 #include <stdio.h>
 #include "predictor.h"
+
 
 //
 // TODO:Student Information
@@ -36,7 +38,10 @@ int verbose;
 //
 //TODO: Add your own Branch Predictor data structures here
 //
-
+int* BHR_tour;      // array to keep track of global t/nt
+int* BHT_global;    // 2^n array to keep track of global t/nt
+int* BHT_local;     // 2^n array to keep track of local t/nt
+int* chooser_tour;  // keeps track of chooser for each addr
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -46,10 +51,46 @@ int verbose;
 //
 void
 init_predictor()
-{
+{ 
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
+  int global_size = pow(2, ghistoryBits);
+  int local_size = pow(2, lhistoryBits);
+  BHT_global = (int*)malloc(sizeof(int) * global_size);
+  BHT_local = (int*)malloc(sizeof(int) * local_size);
+
+  int mem_size = pow(2, ghistoryBits);
+  chooser_tour = (int*)malloc(sizeof(int) * mem_size);
+  printf("%d", ghistoryBits);
+
+  // BHR, branch history register, keeps track of global branch 
+  // taken or not.
+  // all initialized to N/not taken/0
+  int BHR_size = ghistoryBits;
+  BHR_tour = (int*)malloc(sizeof(int) * BHR_size);//  [ghistoryBits];
+  
+  for (int idx = 0; idx < BHR_size; idx++){
+    BHR_tour[idx] = WN;
+  } 
+
+  printf("k%dk\n", BHR_tour[0]);
+  // BHT/PHT, for local history
+  // all initialized to 0/NT
+  //[pcIndexBits][lhistoryBits];
+  int BHT_size = pow(2, pcIndexBits);
+  int loc_size = lhistoryBits;
+  BHT_tour = (int*)malloc(sizeof(int) * BHT_size); 
+  for (int idx = 0; idx < pcIndexBits; idx++){
+    BHT[idx] = malloc(sizeof(int) * loc_size);
+  }
+  
+  
+}
+
+uint8_t make_prediction_tournament(uint32_t pc){
+  printf("%d", pc);
+  return TAKEN;
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -69,6 +110,8 @@ make_prediction(uint32_t pc)
       return TAKEN;
     case GSHARE:
     case TOURNAMENT:
+      //printf("tournament\n");
+      return make_prediction_tournament(pc);
     case CUSTOM:
     default:
       break;
@@ -77,6 +120,8 @@ make_prediction(uint32_t pc)
   // If there is not a compatable bpType then return NOTTAKEN
   return NOTTAKEN;
 }
+
+
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
